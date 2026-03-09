166411671478cb26f0066dfe28df3f0a152cacf8
Claudiu Craciun <ccraciun@ethz.ch>
2026-03-09 22:08:47 +0100

    fixed signal bigwig

--- src/asap/dataloader/utils/data_bw.py
+++ src/asap/dataloader/utils/data_bw.py
@@ -2,13 +2,18 @@ import numpy as np
 
 from .io import get_bw_from_file
 
+def get_coverage(bw, chrom, start, end):
+    chroms_in_bw = bw.chroms()
+    use_chr = any(c.startswith("chr") for c in chroms_in_bw)
+    chrom_query = f'chr{chrom}' if use_chr else f'{chrom}'
+    return bw.values(chrom_query, start, end, numpy=True).astype('float16')
 
 def get_binned_signal(bw_file: str, chrom: int, start: int, end: int, bin_size: int, binning_type: str='max'):
     print(f'Binning signal for chr{chrom}:[{start}, {end}] with mode {binning_type} using {bw_file}')
     assert (end - start) % bin_size == 0, \
         f'Difference between start ({start}) and end ({end}) must be divisible by bin size {bin_size}!'
     bw = get_bw_from_file(bw_file)
-    coverage = bw.values(f'chr{chrom}', start, end, numpy=True).astype('float16')
+    coverage = get_coverage(bw, chrom, start, end)
 
     nans = np.isnan(coverage)
     if nans.sum() > 0:
